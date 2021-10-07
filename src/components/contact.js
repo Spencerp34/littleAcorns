354cc09fb781ac1c97001d380ae86e60edadece1
Spencerp34 <spencerp34@yahoo.com>
2021-10-06 18:09:41 -0600

    finished

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,17 +1,22 @@
 import styled from 'styled-components';
+import profile from '../assets/profile.jpg'
 
 const ContactDiv = styled.div`
-
+    img{
+        border-radius: 50%;
+        width: 20%;
+    }
 `
 
 function Contact(){
     return(
         <ContactDiv>
+            <img src={profile} alt='' />
             <h2> Contact Me</h2>
-            <h3>Email:</h3>
-            <h4>spencerp34@yahoo.com</h4>
-            <h3>Phone:</h3>
-            <h4>(925) 915 - 1853</h4>
+            <h3>✉️Email:</h3>
+            <h4>talia.littleacorns@gmail.com</h4>
+            <h3>📞 Phone:</h3>
+            <h4>(801) 739 - 2830</h4>
             
         </ContactDiv>
     )
--- src/components/glimspe.js
+++ src/components/glimspe.js
@@ -1,6 +1,64 @@
 import styled from 'styled-components';
+import playing from '../assets/playing.jpg'
+import circle from '../assets/circle.jpg'
+import lesson from '../assets/lesson.jpg'
+import singing from '../assets/singing.jpg'
 
 const GlimpseDiv = styled.div`
+
+    p{
+        text-align: left;
+        line-height: 40px;
+    }
+
+    .schedule{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+    }
+    p{
+        width: 40%;
+        min-height: 47.5vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .image{
+        display: flex;
+        width: 45%;
+        height: 40vh;
+        border-radius: 15px;
+        background-position: center;
+        justify-content: center;
+        background-repeat: no-repeat;
+    }
+
+    .one{
+        background-image: url(${playing});
+    }
+    .two{
+        background-image: url(${circle});
+    }
+    .three{
+        background-image: url(${lesson});
+    }
+    .four{
+        background-image: url(${singing});
+    }
+    .last{
+        display: flex;
+        width: 80%;
+        justify-content: right;
+        align-items: center;
+    }
+    .last p{
+        width: 100%;
+    }
+
 
 `
 
@@ -9,7 +67,19 @@ function Glimpse(){
         <div className='Glimpse'>
             <GlimpseDiv>
                 <h1>A Glimpse into our Day to Day!</h1>
-                
+                <div className='schedule'>
+                    <p>Open play - This is an important time for children to learn how to communicate with each other. It allows children to use their creativity and imagination while developing their physical abilities.</p>
+                    <div className='image one'></div>
+                    <div className='image two'></div>
+                    <p>Circle time - This is where we practice our alphabet as well as learn about the day of the week, month, and weather. </p>
+                    <p>Lesson - Whether the lesson is a letter spotlight, learning about emotions, community helpers, or anything in between, we will be working on art and movement and focusing on both fine and gross motor skills. </p>
+                    <div className='image three'></div>
+                    <div className='image four'></div>
+                    <p>Music and stories are a huge focus every day and of course, we cannot forget snack time!</p>
+                    <div className='last'>
+                        <p>Each class will only have up to eight kids so everyone will be able to get tailored assistance.</p>         
+                    </div>
+                </div>
             </GlimpseDiv>
         </div>
     )
